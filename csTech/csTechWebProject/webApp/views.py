6c888d0beef35ec8573df0292058c370cf938334
Nazmus Sakib <sakib@istl.com>
2019-08-20 18:38:28 +0600

    init3

--- csTech/csTechWebProject/webApp/views.py
+++ csTech/csTechWebProject/webApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 
 
 def home(request):
@@ -23,7 +24,7 @@ def tutorial(request):
 
 def registration(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -36,7 +37,7 @@ def registration(request):
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
     requested_page_location = 'webApp/registration.html'
-    form = UserCreationForm()
+    form = NewUserForm()
     return render(request, requested_page_location, context={"form": form})
 
 
